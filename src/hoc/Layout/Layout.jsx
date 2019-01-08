c4a076d2a8fdffb428098a9a110ce56b1a6515c2
Egor <zzzkfjdk@yandex.ru>
2019-01-08 14:37:10 +0300

    add some components

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 import classes from './Layout.css'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle.jsx'
+import Drawer from '../../components/Navigation/Drawer/Drawer.jsx'
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
     render() {
         return (
             <div className={classes.Layout}>
+
+                <Drawer
+                    onClose={this.menuCloseHandler}
+                    isOpen={this.state.menu}
+                />
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
                 <main>
                     {this.props.children}
                 </main>
